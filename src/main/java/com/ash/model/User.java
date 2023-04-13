420aec88123bb9f60c8fe2b9ea7f536e69d5cc50
Алексей Ашутов <i.ashutov@yandex.ru>
2023-04-13 07:51:14 +0300

    init

---
+++ src/main/java/com/ash/model/User.java
@@ -0,0 +1,4 @@
+package com.ash.model;
+
+public record User(Integer id,String name) {
+}
---
+++ src/main/java/com/ash/service/servlet/UserServlet.java
@@ -0,0 +1,28 @@
+package com.ash.service.servlet;
+
+import com.ash.service.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/users")
+public class UserServlet extends HttpServlet {
+
+    private final UserService userService = new UserService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        var writer = resp.getWriter();
+        userService.getAll().forEach(user->{
+            writer.write("""
+                    <h1>%d: %s</h1>""".formatted(user.id(),user.name()));
+        });
+    }
+}
